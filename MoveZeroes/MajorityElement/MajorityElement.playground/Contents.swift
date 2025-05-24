ca5a0753c0d6a9ad4f64ea1f48b380fd59e85373
Benny Kurniawan <bennykurniawan@macbook-pro.tail596430.ts.net>
2025-05-24 14:23:17 +0700

    add majorityElement

---
+++ MoveZeroes/MajorityElement/MajorityElement.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func majorityElement(_ nums: [Int]) -> Int {
+    var dict: [Int: Int] = [:]
+    for num in nums {
+        if let have = dict[num]{
+            dict[num]! += 1
+        } else {
+            dict[num] = 1
+        }
+    }
+    print(dict)
+    var temp = 0
+    var max = 0
+    for (key, value) in dict {
+        print(value)
+        if value > temp {
+            temp = value
+            max = key
+        }
+    }
+    return max
+}
+
+majorityElement([2,2,1,1,1,2,2])
